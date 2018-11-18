75140aaf5f5fb763b74d91b1eb7c40713312ec90
olive sara thomas <olivesara97@gmail.com>
2018-11-19 00:37:54 +0530

    single json

--- file.py
+++ file.py
@@ -3,21 +3,21 @@ import os
 
 class files:
     def __init__(self,fname,mode="r"):
-        filename=str(fname)+".txt"
+        filename=str(fname)+".json"
         if not os.path.isfile(filename):
             self.fptr=open(filename,"w")
-            self.fptr.write("{"+fname+":[]}")
+            self.fptr.write("{}")
             self.fptr.close()
         self.fptr=open(filename,mode)
 
     def fread(self,fname=""):
         self.fptr.seek(0,0)
-        self.data=self.fptr.read()
+        self.data=json.loads(self.fptr.read())
         return self.data
 
     def fwrite(self,matter,fname=""):
-        self.fptr.seek(-2,2)
-        self.fptr.write(matter)
+        self.fptr.seek(0,2)
+        json.dump(matter,self.fptr)
 
     def fclose(self):
         self.fptr.close()
@@ -25,7 +25,7 @@ class files:
 
 x=files("table1","r+")
 print (x.fread())
-x.fwrite("{hi:1,hello:2}")
+x.fwrite({"hi":1,"hello":2})
 print (x.fread())
 x.fclose()
 
