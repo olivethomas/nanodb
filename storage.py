529149e2a2553b057339eb06caf23bacfc3cac3e
olive sara thomas <olivesara97@gmail.com>
2018-11-19 13:24:11 +0530

    backend upload

---
+++ storage.py
@@ -0,0 +1,57 @@
+import json
+import os
+from collections import OrderedDict
+
+class storage:
+    def __init__(self,fname,mode="r"):
+        filename=str(fname)+".json"
+        if not os.path.isfile(filename):
+            self.fptr=open(filename,"w")
+            #self.fptr.write("{}")
+            self.fptr.close()
+        #self.fptr=open(filename,mode)
+
+    def fopen(self,fname,mode="r"):
+        filename=str(fname)+".json"
+        if not os.path.isfile(filename):
+            return 0
+        else:
+            self.fptr=open(filename,mode)
+            return 1
+
+    def fread(self,fname):
+        if self.fopen(fname)==0:
+            return -1
+        self.fptr.seek(0,0)
+        #self.data=json.loads(self.fptr.read())
+        try:
+            self.data = json.loads(self.fptr.read(), object_hook=OrderedDict)
+        except ValueError:
+            self.data = OrderedDict()
+        self.fclose()
+        return self.data
+
+    def fwrite(self,matter,fname,mode="r+"):
+        data=self.fread(fname)
+        if self.fopen(fname,mode)==0:
+            return
+        self.fptr.seek(0,0)
+        data.update(matter)
+        print (data)
+        #self.fptr.seek(0,2)
+        json.dump(data,self.fptr)
+        self.fclose()
+
+    def fclose(self):
+        self.fptr.close()
+
+
+x=files("table1","r+")
+print (x.fread("table1"))
+x.fwrite({3 : {"hi":1,"hello":2} }, "table1")
+#x.fwrite(OrderedDict([('hi1', 1), ('hello1', 2)]))
+print (x.fread("table1"))
+x.fclose()
+
+        
+        
